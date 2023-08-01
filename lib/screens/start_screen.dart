8a6d240776c11b4b76001d6f9a69d7fd6452b061
Md Shoyab <Mdshoyab06@gmail.com>
2023-08-01 20:53:35 +0530

    start screen completed

--- lib/screens/start_screen.dart
+++ lib/screens/start_screen.dart
@@ -5,8 +5,40 @@ class StartScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Start Screen'),
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Image.asset(
+            'assets/images/quiz_logo.png',
+            width: 200,
+          ),
+          const SizedBox(height: 80),
+          const Padding(
+            padding: EdgeInsets.symmetric(horizontal: 50),
+            child: Text(
+              'Learn Anything from the Fun of Quiz App!',
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                fontSize: 25,
+                color: Colors.white,
+                fontWeight: FontWeight.w500,
+                height: 1.5,
+              ),
+            ),
+          ),
+          const SizedBox(height: 50),
+          OutlinedButton.icon(
+            onPressed: () {},
+            style: OutlinedButton.styleFrom(
+              foregroundColor: const Color.fromARGB(255, 247, 59, 2),
+              backgroundColor: Colors.white,
+            ),
+            icon: const Icon(Icons.keyboard_arrow_right),
+            label: const Text('Start Quiz!'),
+          ),
+        ],
+      ),
     );
   }
 }
