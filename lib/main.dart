9f5beb5caad03ac2cb4a46f747f0ee0bc8fe0a8b
Md Shoyab <Mdshoyab06@gmail.com>
2023-08-23 00:59:02 +0530

    Added Quiz Widget inside runApp

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_quiz_app/screens/start_screen.dart';
+import 'package:flutter_quiz_app/screens/quiz.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
-      home: Scaffold(
-        body: Container(
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-              colors: [
-                Color.fromARGB(255, 247, 59, 2),
-                Color.fromARGB(255, 241, 90, 44),
-              ],
-              begin: Alignment.topLeft,
-              end: Alignment.bottomRight,
-            ),
-          ),
-          child: const StartScreen(),
-        ),
-      ),
-    ),
-  );
+  runApp(const Quiz());
 }
