6062ad557c3f8b3e1855cc30379db0fd1a86aec8
Md Shoyab <Mdshoyab06@gmail.com>
2023-08-23 00:56:35 +0530

    Created Question screen class and relevant code into it.

---
+++ lib/screens/question_screen.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class QuestionScreen extends StatefulWidget {
+  const QuestionScreen({super.key});
+
+  @override
+  State<QuestionScreen> createState() => _QuestionScreenState();
+}
+
+class _QuestionScreenState extends State<QuestionScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+      child: Text('Question Screen'),
+    );
+  }
+}
