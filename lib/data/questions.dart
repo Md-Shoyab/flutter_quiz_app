2a843572e6f8d0db55f84d5417630665208254b8
Md Shoyab <Mdshoyab06@gmail.com>
2023-09-10 19:22:09 +0530

    Dummy Questions Data Added

---
+++ lib/data/questions.dart
@@ -0,0 +1,59 @@
+import 'package:flutter_quiz_app/models/quiz_questions.dart';
+
+const questions = [
+  QuizQuestions(
+    'What are the main building block of Flutter UIs?',
+    [
+      'Widgets',
+      'Components',
+      'Blocks',
+      'Functions',
+    ],
+  ),
+  QuizQuestions(
+    'How are Flutter UIs built?',
+    [
+      'By combining widgets in code',
+      'By combining widgets in a visual editor',
+      'By defining widgets in config files',
+      'By using Xcode for iOS and Android Studio for androd',
+    ],
+  ),
+  QuizQuestions(
+    'What\'s the purpose of a StatefulWidget?',
+    [
+      'Update UI as data changes',
+      'Update data as UI changes',
+      'Ignore data changes',
+      'Render UI that doesn\'t depends on data',
+    ],
+  ),
+
+    QuizQuestions(
+    'Which widget should you try to use more often: StatelessWidget or StatefulWidget',
+    [
+      'StatelessWidget',
+      'StatefulWidget',
+      'Both are equally good',
+      'None of the above',
+    ],
+  ),
+  QuizQuestions(
+    'How should you update data inside of StatefulWidgets?',
+    [
+      'By calling setState()',
+      'By calling updateData()',
+      'By calling updateUI()',
+      'By calling updateState()',
+    ],
+  ),
+  QuizQuestions(
+    'What happens if you change data in a StatelessWidget?',
+    [
+      'The UI is not updated',
+      'The UI is updated',
+      'The closest StatefulWidget is updated',
+      'Any nested StatefulWidgets are updated',
+    ],
+  ),
+];
