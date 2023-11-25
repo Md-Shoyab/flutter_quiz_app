bb3fb475d0e9a56aa1c7d2b09908ef9c468d4dda
Md Shoyab <Mdshoyab06@gmail.com>
2023-11-25 18:04:33 +0530

    Resolved Row overflow with the use of Expanded on result screen.

--- lib/widgets/questions_summary.dart
+++ lib/widgets/questions_summary.dart
@@ -13,13 +13,15 @@ class QuestionSummary extends StatelessWidget {
           return Row(
             children: [
               Text(((data['question_index'] as int) + 1).toString()),
-              Column(
-                children: [
-                  Text((data['question'] as String)),
-                  const SizedBox(height: 5.0),
-                  Text((data['user_answer'] as String)),
-                  Text((data['correct_answer'] as String)),
-                ],
+              Expanded(
+                child: Column(
+                  children: [
+                    Text((data['question'] as String)),
+                    const SizedBox(height: 5.0),
+                    Text((data['user_answer'] as String)),
+                    Text((data['correct_answer'] as String)),
+                  ],
+                ),
               ),
             ],
           );
