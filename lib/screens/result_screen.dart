b29e0882355b6102e0b708526ea85457ab90127e
Md Shoyab <Mdshoyab06@gmail.com>
2023-11-25 15:38:04 +0530

    Set text alignment to center on the result screen.

--- lib/screens/result_screen.dart
+++ lib/screens/result_screen.dart
@@ -10,11 +10,18 @@ class ResultScreen extends StatelessWidget {
       child: Container(
         margin: const EdgeInsets.all(40),
         child: Column(
-          
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
           children: [
-            const Text('You have answered x out of y questions correctly!'),
+            const Text(
+              'You have answered x out of y questions correctly!',
+              textAlign: TextAlign.center,
+            ),
             const SizedBox(height: 30),
-            const Text('List of answers and questions'),
+            const Text(
+              'List of answers and questions',
+              textAlign: TextAlign.center,
+            ),
             const SizedBox(height: 30),
             TextButton(
               onPressed: () {},
